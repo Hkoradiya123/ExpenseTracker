52696dcca42e7ffd04d9755ff4f490d323d3c081
MalharBhatt <127686254+MalharBhatt99@users.noreply.github.com>
2025-02-25 01:02:47 +0530

    COMMIT 2 DASHBOARD NAVIGATION BY MALHAR

--- app/src/main/java/com/example/expensetracker/HomeDetailsActivity.java
+++ app/src/main/java/com/example/expensetracker/HomeDetailsActivity.java
@@ -1,21 +1,27 @@
 package com.example.expensetracker;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.util.Log;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.*;
@@ -24,16 +30,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HomeDetailsActivity extends AppCompatActivity {
+public class HomeDetailsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ListView customerListView;
     Button addCustomerButton;
-    TextView headerText;
+
     ArrayList<String> customers;
     ArrayAdapter<String> adapter;
     FirebaseFirestore db;
 
     FirebaseUser currentUser;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +54,19 @@ public class HomeDetailsActivity extends AppCompatActivity {
         // Initialize Views
         customerListView = findViewById(R.id.customerListView);
         addCustomerButton = findViewById(R.id.addCustomerButton);
-        headerText = findViewById(R.id.headerText);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        //toolbar
+        setSupportActionBar(toolbar);
+
+        //navigationview
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
         // Initialize Firestore
         db = FirebaseFirestore.getInstance();
@@ -82,6 +105,15 @@ public class HomeDetailsActivity extends AppCompatActivity {
             showDeleteCustomerDialog(position);
             return true;
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private void loadCustomers() {
@@ -283,5 +315,10 @@ public class HomeDetailsActivity extends AppCompatActivity {
                     Toast.makeText(this, "Error deleting customer", Toast.LENGTH_SHORT).show();
                     Log.e("HomeDetailsActivity", "Error deleting customer", e); // Log the error for debugging
                 });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
